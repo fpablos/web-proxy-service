b51091fe6542fc13e157ca6a180ac7116aedefa6
Federico Pablos <federico.pablos@valtech.com>
2020-04-12 21:39:09 -0300

    Improved ips handling

--- filter-chain/utils.go
+++ filter-chain/utils.go
@@ -7,23 +7,30 @@ import (
 	s "strings"
 )
 
-func getIp(r *http.Request) string{
+func GetIp(r *http.Request) string{
+	// X-Forwarded-For: <client>, <proxy1>, <proxy2>
 	forwarded := r.Header.Get("X-FORWARDED-FOR")
 	if forwarded != "" {
-		return s.Split(forwarded, ":")[0]
+		return s.Split(forwarded, ",")[0]
+	}
+	// It has "IP:port" format
+	if s.Contains(r.RemoteAddr, "[") {
+		return s.Split(r.RemoteAddr, "]")[0]+"]"
 	}
 	return s.Split(r.RemoteAddr, ":")[0]
 }
 
-func getHostIp(r *http.Request) string{
+func GetHostIp(r *http.Request) string{
 	addrs,err := net.LookupIP(r.Host)
 	if err != nil {
 		fmt.Println("Unknown host")
+		// I take it as error
+		return ""
 	}
 	// We returns the first in the list of ip's
 	return addrs[0].String()
 }
 
-func getPath(r *http.Request) string{
+func GetPath(r *http.Request) string{
 	return r.URL.Path
 }
